5d7b80c906ca3abbf8f669d5267b2f4dd1e9f1ed
sau-c <sau.contacto@gmail.com>
2025-06-25 12:08:50 -0600

    Vista CrearRol

--- src/PortalCOSIE.Domain/Entities/Rol.cs
+++ src/PortalCOSIE.Domain/Entities/Rol.cs
@@ -12,5 +12,6 @@ namespace PortalCOSIE.Domain.Entities
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
+        public string Descripcion { get; set; }
     }
 }
